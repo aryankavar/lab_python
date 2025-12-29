ee8c900f596bb411b2a4748014b5c7d8e8f03595
aryan <aryandk84605@gmail.com>
2025-12-29 23:07:16 +0530

    update

--- oparator.py
+++ oparator.py
@@ -1,4 +1,24 @@
 # 1. assignment operator
 a = 10
 print(a)
+
 # 2. unary operator
+b = 5
+print(-b)
+
+# 3. relational operator
+c = 10
+d = 20
+print(c < d)
+print(c==d)
+
+# 4. logical operator
+x = True
+y = False
+print(x and y)
+print(x or y)
+print(not x)
+
+# 5. boolean operator
+is_pass = True
+is_fail = False 
