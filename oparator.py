e7560ada4f7dd53806cc76d85c28fea3e009e8f5
aryan <aryandk84605@gmail.com>
2025-12-31 08:50:44 +0530

    update

--- oparator.py
+++ oparator.py
@@ -21,4 +21,21 @@ print(not x)
 
 # 5. boolean operator
 is_pass = True
-is_fail = False 
+print(is_pass)
+
+# 6. bitwise operator
+a = 5
+b = 3
+print (a&b) # AND
+print (a|b) # OR
+
+# 7. membership 
+numbers = [1,2,3,4,5]
+print(2 in numbers)
+print(5 not in numbers)
+
+# 8. identity operator
+x = 10
+y = 10
+print(x is y)
+print(x is not y)
