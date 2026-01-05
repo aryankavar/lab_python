5628fc61413971a4cfb77c8022055e8c7cdbbbce
aryan <aryandk84605@gmail.com>
2026-01-05 10:15:13 +0530

    new

---
+++ data_type.py
@@ -0,0 +1,61 @@
+# string
+str="Hello, World!"
+print(str)
+
+# integer
+num=42
+print(num)
+
+# float
+flt=3.14
+print(flt)      
+
+# boolean
+bool_val=True
+print(bool_val)
+
+# list
+lst=[1,2,3,4,5]
+print(lst)
+
+# tuple
+tpl=(1,2,3,4,5)
+print(tpl)
+
+# dictionary
+dct={"name":"Alice","age":30}
+print(dct)
+
+# set
+st={1,2,3,4,5}
+print(st)
+
+# None
+none_val=None
+print(none_val)
+
+# complex number
+cplx=2+3j
+print(cplx)
+
+# bytes
+bts=b"Hello"
+print(bts)
+# bytearray
+byts=bytearray(b"Hello")
+print(byts)
+# bytearray
+barr=bytearray(b"Hello")
+print(barr)
+
+# memoryview
+mv=memoryview(b"Hello")
+print(mv)
+
+# range
+rng=range(5)
+print(rng)
+
+# frozenset
+fs=frozenset({1,2,3,4,5})
+print(fs)
