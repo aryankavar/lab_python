83f9987d64da55bd001d71564a55085e8d358e3a
aryan <aryandk84605@gmail.com>
2026-02-02 10:19:43 +0530

    I/O

---
+++ IO.py
@@ -0,0 +1,24 @@
+# name = input("Enter your Name : ")
+# city = input("Enter your city : ")
+
+# print(name, "is from", city)
+
+# num1=int(input("Enter first number : "))
+# num2=int(input("Enter second number : "))
+
+# print("the sum is ", num1+num2)
+
+# num3=float(input("Enter float number :"))
+# num4=float(input("Enter another float number :"))
+
+# print("The substraction is ", num3 - num4)
+
+# bool_1 = bool(input("Enter first boolean value (True/False) : "))
+# bool_2 = bool(input("Enter second boolean value (True/False) : "))
+
+# print("The logical AND is ", bool_1 and bool_2)
+
+bool_1 = input("Enter first boolean value (True/False): ").strip()
+bool_2 = input("Enter second boolean value (True/False): ").strip()
+
+print("The logical AND is", bool_1 and bool_2)
