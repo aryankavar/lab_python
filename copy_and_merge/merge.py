72bc0765414650c664b621a0577fae8f0b918b7e
aryan <aryandk84605@gmail.com>
2026-05-04 08:44:45 +0530

    Update via AutoApp

--- copy_and_merge/merge.py
+++ copy_and_merge/merge.py
@@ -1,5 +1,19 @@
-with open("introduction.txt", "r") as f1,open("introduction_copy.txt", "w") as f2,open("output.txt", "w") as f3:
+'''with open("introduction.txt", "r") as f1,open("introduction_copy.txt", "w") as f2,open("output.txt", "w") as f3:
     f3.write(f1.read())
     f3.write("\n")
     f3.write(f1.read())
+print("Files merged successfully.")'''
+
+
+
+f1 = open("abc.txt", "r")
+f2 = open("xyz.txt", "r")
+f3 = open("output.txt", "a")
+
+f3.write(f1.read())
+f3.write(f2.read())
+
+f1.close()
+f2.close()
+f3.close()
 print("Files merged successfully.")
--- try,except,else,finally_block/try_and_except.py
+++ try,except,else,finally_block/try_and_except.py
@@ -4,9 +4,7 @@ try:
     result = num1 / num2
 except ZeroDivisionError:
     print("Error: Cannot divide by zero.")
-except ValueError:
-    print("Error: Invalid input. Please enter integers.")
 else:
-    print(f"The result of {num1} divided by {num2} is: {result}")
+    print("The result is:", result)
 finally:
     print("Execution completed.")
